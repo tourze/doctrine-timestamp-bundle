2fe4d7f1d070d35d589e3f6cb31557f457dc9347
github-ci <you@example.com>
2025-06-14 00:52:54 +0800

    移除 `CreateTimeAware` trait 中的索引注解以优化代码结构。

--- src/Traits/CreateTimeAware.php
+++ src/Traits/CreateTimeAware.php
@@ -4,12 +4,10 @@ namespace Tourze\DoctrineTimestampBundle\Traits;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Tourze\DoctrineIndexedBundle\Attribute\IndexColumn;
 use Tourze\DoctrineTimestampBundle\Attribute\CreateTimeColumn;
 
 trait CreateTimeAware
 {
-    #[IndexColumn]
     #[CreateTimeColumn]
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true, options: ['comment' => '创建时间'])]
     private ?\DateTimeInterface $createTime = null;
